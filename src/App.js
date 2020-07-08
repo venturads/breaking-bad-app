c7017330b039bdb7e2cbde75f86eea408f2faefc
venturads <albert@venturads.com>
2020-07-08 11:28:02 -0700

    added search with props

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import axios from 'axios';
 import Header from './components/ui/Header';
 import './App.css';
 import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
 
 function App() {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
 
       console.log(result.data);
       setItems(result.data);
@@ -18,10 +20,11 @@ function App() {
     }
 
     fetchItems();
-  })
+  }, [query])
   return (
     <div className="App">
       <Header />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
