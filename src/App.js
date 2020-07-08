53c624ce20abfac3b8dd56053468fc0570e108fd
venturads <albert@venturads.com>
2020-07-08 10:48:55 -0700

    add hooks call all data from cast

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from './components/ui/Header';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+      console.log(result.data);
+    }
+
+    fetchItems();
+  })
   return (
     <div className="App">
       <Header />
